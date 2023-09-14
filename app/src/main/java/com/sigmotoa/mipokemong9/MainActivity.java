e1af01301ad7ecdd9d1ac70cc1f9b6fb1f6b264f
sigmotoa <11698886+sigmotoa@users.noreply.github.com>
2023-09-14 09:24:09 -0500

    Se adiciona scrollview en lateral Se cambia el icono de la app

--- app/src/main/java/com/sigmotoa/mipokemong9/MainActivity.java
+++ app/src/main/java/com/sigmotoa/mipokemong9/MainActivity.java
@@ -40,6 +40,63 @@ public class MainActivity extends AppCompatActivity {
                 .fitCenter()
                 .into(imgPokemon5);
 
+        //Adicionando las imagenes al scrollvertical
+
+        ImageView imgPk1 = findViewById(R.id.img_pokemon_mini_1);
+        ImageView imgPk2 = findViewById(R.id.img_pokemon_mini_2);
+        ImageView imgPk3 = findViewById(R.id.img_pokemon_mini_3);
+        ImageView imgPk4 = findViewById(R.id.img_pokemon_mini_4);
+        ImageView imgPk5 = findViewById(R.id.img_pokemon_mini_5);
+        ImageView imgPk6 = findViewById(R.id.img_pokemon_mini_6);
+        ImageView imgPk7 = findViewById(R.id.img_pokemon_mini_7);
+        ImageView imgPk8 = findViewById(R.id.img_pokemon_mini_8);
+        ImageView imgPk9 = findViewById(R.id.img_pokemon_mini_9);
+        ImageView imgPk10 = findViewById(R.id.img_pokemon_mini_10);
+
+        Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif")
+                .fitCenter()
+                .into(imgPk1);
+  Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/2.gif")
+                .fitCenter()
+                .into(imgPk2);
+  Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/3.gif")
+                .fitCenter()
+                .into(imgPk3);
+  Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/4.gif")
+                .fitCenter()
+                .into(imgPk4);
+  Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/5.gif")
+                .fitCenter()
+                .into(imgPk5);
+  Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/6.gif")
+                .fitCenter()
+                .into(imgPk6);
+  Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/7.gif")
+                .fitCenter()
+                .into(imgPk7);
+  Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/8.gif")
+                .fitCenter()
+                .into(imgPk8);
+  Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/9.gif")
+                .fitCenter()
+                .into(imgPk9);
+  Glide.with(this)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/10.gif")
+                .fitCenter()
+                .into(imgPk10);
+
+
+
+
 
     }
 }
